76c2d214b1df63cf5783bba5b2196da8860c5932
shrawanamit <amitkumar06111@gmail.com>
2020-04-25 01:21:42 +0530

    userRegistration. [UseCase 3] validate email

--- userRegistration.sh
+++ userRegistration.sh
@@ -5,7 +5,7 @@ function validateFirstName(){
 
 	echo "enter your name"
 	read firstName
-	pattern=[A-Z][a-z]{3,}
+	pattern=^[A-Z][a-z]{3,}
 
 	if [[ $firstName =~ $pattern ]]
 	then
@@ -20,7 +20,7 @@ function validateLastName(){
 
 	echo "enter your last name"
 	read lastName
-	pattern=[A-Z][a-z]{3,}
+	pattern=^[A-Z][a-z]{3,}
 
 	if [[ $lastName =~ $pattern ]]
 	then
@@ -30,3 +30,19 @@ function validateLastName(){
 	fi
 }
 validateLastName
+
+function validateEmail(){
+
+	 echo "enter your email"
+	 read email
+	
+	pattern="^[a-zA-Z0-9]+([._-+][a-zA-Z0-9]+)*@[a-zA-Z]+.[a-zA-Z]{2,4}([.][a-zA-Z]{2})$"
+
+	if [[ $email =~ $pattern ]]
+	then
+ 	echo "valid email"
+ 	else
+ 	echo "invalid email"
+	fi
+}
+validateEmail
